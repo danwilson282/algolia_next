1928a99db84a3d7dbf2fa974fbdd88d00ceec499
Dan Wilson <danwilson282@yahoo.co.uk>
2024-01-15 14:45:12 +0000

    nearly branded

--- app/Search.tsx
+++ app/Search.tsx
@@ -6,9 +6,17 @@ import React from 'react';
 import {
   SearchBox,
   RefinementList,
+  useRefinementList,
+  UseRefinementListProps,
   DynamicWidgets,
   useHits,
   UseHitsProps,
+  useMenu,
+  UseMenuProps,
+  ClearRefinements,
+  SortBy,
+  Pagination,
+  HitsPerPage,
 } from 'react-instantsearch';
 import { InstantSearchNext } from 'react-instantsearch-nextjs';
 
@@ -26,6 +34,30 @@ type HitProps = {
   }>;
 };
 
+function MenuSelect(props: UseMenuProps) {
+  const { items, refine } = useMenu(props);
+  const { value: selectedValue } = items.find((item) => item.isRefined) || {
+    value: '',
+  };
+
+  return (
+    <select
+      value={selectedValue}
+      onChange={(event) => {
+        document.getElementById('clear')?.children[0].click()
+        refine((event.target as HTMLSelectElement).value);
+      }}
+    >
+      <option value="">Show all</option>
+      {items.map((item) => (
+        <option value={item.value}>
+          {item.label} ({item.count})
+        </option>
+      ))}
+    </select>
+  );
+}
+
 function CustomHits(props: UseHitsProps) {
   const { hits } = useHits(props);
 
@@ -41,13 +73,13 @@ function CustomHits(props: UseHitsProps) {
               </tr>
             </thead>
             <tbody>
-              {hits.map((hit:any):React.ReactNode => (
+              {hits.map((hit:AlgoliaHit):React.ReactNode => (
                 <tr>
                 <td><span className="Hit-headword">{hit.HEADWORD}</span></td>
                 <td><span className="Hit-english">{hit.ENGLISH_EQUIVALENT}</span></td>
                 <td><span className="Hit-part">{hit.PART_OF_SPEECH}</span></td>
                 <td><span className="Hit-tier">{hit.TIER_DISPLAY}</span></td>
-                <td><span className="Hit-part">{hit.FILENAME}</span></td>
+                <td><a href={hit.FILENAME} className="Hit-part">Download</a></td>
               </tr>
               ))}
             </tbody>
@@ -55,17 +87,139 @@ function CustomHits(props: UseHitsProps) {
   );
 }
 
+function CustomRefinementList(props: UseRefinementListProps) {
+  const {
+    items,
+    refine,
+    searchForItems,
+    canToggleShowMore,
+    isShowingMore,
+    toggleShowMore,
+  } = useRefinementList(props);
+
+  return (
+    <>
+      <ul className="o-list-bare u-mb0">
+        {items.map((item) => (
+          <li key={item.label}>
+            <div className="row">
+              <div className="col-xs col-xs-1">
+              <input
+                type="checkbox"
+                checked={item.isRefined}
+                onChange={() => refine(item.value)}
+              />
+              </div>
+              <div className="col-xs col-xs-11">
+                <span>{item.label}</span>
+                <span> ({item.count})</span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 export default function Search() {
   return (
     <InstantSearchNext searchClient={client} indexName="aqa-audio-files" routing>
       <div className="Container row">
-        <div className="col col-xs-4">
-          <SearchBox />
-          <DynamicWidgets fallbackComponent={FallbackComponent} />
+        <div className="col-xs col-xs-4 bg-grey-lightest">
+        <SearchBox className="c-searchbox__field col-xs col-xs-12" placeholder="Search audio files" />
+          <div className="c-modal--overlay__container">
+            <div className="c-accordion u-sm-p1-5">
+              <div className="row">
+                <div className="col-xs u-mt1">
+                  <ClearRefinements></ClearRefinements>
+                </div>
+              </div>
+            <div className="panel-group flb-panel">
+              <dl className="c-accordion">
+                <div className="panel panel-default">
+                  <dt className="panel-heading">
+                    <span className="c-accordion__title c-accordion__title__btn is-expanded">Theme 1: People and lifestyle</span>
+                  </dt>
+                  <dd className="c-accordion__item is-scrollable is-expanded bg-color-white">
+                  <MenuSelect attribute="A_PEOPLE_AND_LIFESTYLE"/>
+                  </dd>
+                </div>
+                <div className="panel panel-default">
+                  <dt className="panel-heading">
+                    <span className="c-accordion__title c-accordion__title__btn is-expanded">Theme 2: Popular culture</span>
+                  </dt>
+                  <dd className="c-accordion__item is-scrollable is-expanded bg-color-white">
+                  <MenuSelect attribute="B_POPULAR_CULTURE"/>
+                  </dd>
+                </div>
+                <div className="panel panel-default">
+                  <dt className="panel-heading">
+                    <span className="c-accordion__title c-accordion__title__btn is-expanded">Theme 3: Communication</span>
+                  </dt>
+                  <dd className="c-accordion__item is-scrollable is-expanded bg-color-white">
+                  <MenuSelect attribute="C_COMMUNICATION"/>
+                  </dd>
+                </div>
+                <div className="panel panel-default">
+                  <dt className="panel-heading">
+                    <span className="c-accordion__title c-accordion__title__btn is-expanded">General</span>
+                  </dt>
+                  <dd className="c-accordion__item is-scrollable is-expanded bg-color-white">
+                    <CustomRefinementList attribute="GENERAL"/>
+                  </dd>
+                </div>
+                <div className="panel panel-default">
+                  <dt className="panel-heading">
+                    <span className="c-accordion__title c-accordion__title__btn is-expanded">Cultural</span>
+                  </dt>
+                  <dd className="c-accordion__item is-scrollable is-expanded bg-color-white">
+                    <CustomRefinementList attribute="CULTURAL"/>
+                  </dd>
+                </div>
+                <div className="panel panel-default">
+                  <dt className="panel-heading">
+                    <span className="c-accordion__title c-accordion__title__btn is-expanded">Tier</span>
+                  </dt>
+                  <dd className="c-accordion__item is-scrollable is-expanded bg-color-white">
+                    <CustomRefinementList attribute="TIER"/>
+                  </dd>
+                </div>
+                <div className="panel panel-default">
+                  <dt className="panel-heading">
+                    <span className="c-accordion__title c-accordion__title__btn is-expanded">Part of speech</span>
+                  </dt>
+                  <dd className="c-accordion__item is-scrollable is-expanded bg-color-white">
+                    <CustomRefinementList attribute="PART_OF_SPEECH"/>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+            </div>
+          </div>
+          <ClearRefinements id="clear" style={{display:'none'}} includedAttributes={['A_PEOPLE_AND_LIFESTYLE', 'B_POPULAR_CULTURE', 'C_COMMUNICATION']}></ClearRefinements>
+          <SortBy
+            items={[
+              { label: 'Word', value: 'headword_asc' },
+              { label: 'Translation', value: 'trans_asc' },
+              { label: 'Part of speech', value: 'part_asc' },
+              { label: 'Tier', value: 'tier_asc' },
+            ]}
+          />
+          
         </div>
         <div className="col col-xs-8">
+            <HitsPerPage
+              items={[
+               { label: '10 hits per page', value: 10 },
+               { label: '20 hits per page', value: 20 },
+               { label: '50 hits per page', value: 50, default: true },
+               { label: '100 hits per page', value: 100 },
+              ]}
+            />
             <CustomHits />
+            <Pagination/>
+            
         </div>
       </div>
     </InstantSearchNext>
